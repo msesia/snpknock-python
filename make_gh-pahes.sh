e3a4bb141c761b80adf335a5bb62b2b43633ca90
Matteo Sesia <msesia@stanford.edu>
2019-05-15 19:13:45 -0700

    Added make for docs

---
+++ make_gh-pahes.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+GH_PAGES_SOURCES="docs/source docs/Makefile"
+
+git checkout gh-pages
+rm -rf build _sources _static _modules
+git checkout master $GH_PAGES_SOURCES
+git reset HEAD
+cd docs
+make html
+cd ..
+mv -fv docs/build/html/* ./
+rm -rf $GH_PAGES_SOURCES docs/build
+
+git add -A
+git commit -m "Generated gh-pages for `git log master -1 --pretty=short --abbrev-commit`"
+git push origin gh-pages
+#git checkout master
+#git reset HEAD --hard
